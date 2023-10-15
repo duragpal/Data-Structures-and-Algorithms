7e1126319633e10f821074b476385ea19e065e5c
Durag Pal Singh <107455742+duragpal@users.noreply.github.com>
2023-10-15 14:37:49 +0530

    Create 3-InsertEnd.c

---
+++ 2.1 Linked list/2. Doubly Linked List/3-InsertEnd.c
@@ -0,0 +1,65 @@
+// Online C compiler to run C program online
+#include<stdio.h>
+#include<stdlib.h>
+#include<malloc.h>
+typedef struct node{
+    int data;
+    struct node* next;
+    struct node* prev;
+} Node;
+
+// Node* insertBegin(Node* head,int x){
+//     Node *temp=(struct node*)malloc(sizeof(struct node));
+//     temp->data=x;
+//     temp->next=head;
+//     if(head!=NULL)
+//     head->prev=temp;
+//     else
+//     head->prev=NULL;
+//     return temp;
+    
+    
+// }
+Node* insertEnd(Node* head,int x){
+   Node *temp=(struct node*)malloc(sizeof(struct node));
+   temp->data=x;
+   temp->next=NULL;
+   temp->prev=NULL;
+   if(head==NULL)
+   return temp;
+   Node*ptr=head;
+   while(ptr->next!=NULL)
+   ptr=ptr->next;
+   ptr->next=temp;
+   temp->prev=head;
+   return head;
+    
+}
+
+int main() {
+    Node *head=NULL;
+    head=(struct node*)malloc(sizeof(struct node));
+    head->data=45;
+    head->prev=NULL;
+    head->next=(struct node*)malloc(sizeof(struct node));
+    head->next->next=(struct node*)malloc(sizeof(struct node));
+    head->next->data=35;
+    head->next->prev=head;
+    head->next->next->data=25;
+    head->next->next->prev=head->next;
+    head->next->next->next=NULL;
+    
+    struct node* ptr;
+    ptr=head;
+    while(ptr!=NULL){
+    printf("%d ", ptr->data);
+    ptr=ptr->next;
+    }
+    printf("\nAfter insertion at Beginning:\n ");
+    ptr=insertEnd(head,75);
+    while(ptr!=NULL){
+    printf("%d ", ptr->data);
+    ptr=ptr->next;
+    }
+    return 0;
+}
