40f46f8b2bc7c0d951371381f019f130e8c14d4b
Durag Pal Singh <107455742+duragpal@users.noreply.github.com>
2023-10-16 17:44:20 +0530

    Create 3-insertend.c

---
+++ 2.1 Linked list/3. Circular Linked List/3-insertend.c
@@ -0,0 +1,72 @@
+
+#include <stdio.h>
+#include<malloc.h>
+
+typedef struct node{
+    int data;
+    struct node* next;
+}Node;
+
+// Node *insertBegin(Node* head,int data){
+//     Node *temp=(struct node*)malloc(sizeof(struct node));
+//     temp->data=data;
+//     if (head==NULL){
+//     temp->next = temp;
+//         head=temp;
+//     }
+//     else{
+//         temp->next=head->next;
+//         head->next=temp;
+//         int x= head->data;
+//         head->data=temp->data;
+//         temp->data=x;
+        
+//     }
+//     return head;
+Node * insertEnd(Node * head, int data){
+    Node *temp=(struct node*)malloc(sizeof(struct node));
+    temp->data=data;
+    if(head==NULL)
+    {
+        temp->next=temp;
+        
+        
+    }
+    else{
+        temp->next=head->next;
+        head->next=temp;
+        int t=temp->data;
+        temp->data=head->data;
+        head->data=t;
+        
+    }
+    return temp;
+}
+    
+    
+
+int main()
+{ 
+    Node *head=(Node*)malloc(sizeof(Node));
+    head->data=35;
+    head->next=(Node*)malloc(sizeof(Node));
+    head->next->data=96;
+    head->next->next=(Node*)malloc(sizeof(Node));
+    head->next->next->data=55;
+    head->next->next->next=head;
+    
+    struct node* ptr;
+    ptr=head;
+    do{
+        printf("%d ",ptr->data);
+        ptr=ptr->next;
+    }while(ptr!=head);
+    head=insertEnd(head,66);
+    ptr=head;
+printf("\nAfter Insertion.\n");
+do{
+        printf("%d ",ptr->data);
+        ptr=ptr->next;
+    }while(ptr!=head);
+    return 0;
+}
